95535cee3334db2c16ebae556e5f52bee40392b0
SonicWu <hibigelf@gmail.com>
2013-06-15 12:07:52 +0800

    Add: Sign in with Sina Weibo

--- flask/main.py
+++ flask/main.py
@@ -1,9 +1,14 @@
-from flask import Flask, url_for, render_template
+from flask import Flask, url_for, render_template, request
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/wb_callback")
+def weibo_callback():
+    return render_template("weibo_callback.html")
+
 
 if __name__ == "__main__":
     #app.debug = True
