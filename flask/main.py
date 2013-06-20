664904b472c5a7f4c3aae21aacd66faa289c2125
SonicWu <hibigelf@gmail.com>
2013-06-20 22:37:10 +0800

    Add: user logout.

--- flask/main.py
+++ flask/main.py
@@ -1,6 +1,5 @@
 # –*- encoding:utf8 –*-
 from flask import Flask, url_for, redirect, render_template, request, make_response, session
-import urllib2
 import uuid
 import json
 import config
@@ -29,33 +28,30 @@ def index():
 @app.route('/login', methods=['POST'])
 def login():
     source = request.form['source']
-    platform_uid = request.form['platform_uid']
     access_token = request.form['access_token']
 
-    # Get user data from platform.
-    platform_url = 'https://api.weibo.com/2/users/show.json?access_token={access_token}&uid={uid}' \
-        .format(access_token=access_token, uid=platform_uid)
-    platform_res = urllib2.urlopen(platform_url)
-    platform_res_body = platform_res.read()
-    platform_user = json.loads(platform_res_body)
-
-    if platform_user is None or 'screen_name' not in platform_user:
+    platform_data = json.loads(request.form['platform_data'])
+    if platform_data is None:
         return ''
 
-    pid = '{source}_{platform_uid}'.format(source=source, platform_uid=platform_uid)
-    pid_uid_key = 'platform_uid:{pid}:uid'.format(pid=pid)
+    platform_user = platform_data['data']
+    if platform_user is None or 'id' not in platform_user or 'screen_name' not in platform_user:
+        return ''
+
+    pid = '{source}_{platform_uid}'.format(source=source, platform_uid=platform_user['id'])
+    pid_uid_key = 'platform_uid:to:uid'
 
     conn = db.get_conn()
-    uid = conn.get(pid_uid_key)
+    uid = conn.hget(pid_uid_key, pid)
     if uid == None:
         uid = str(uuid.uuid1()).upper()
-        conn.set(pid_uid_key, uid)
+        conn.hset(pid_uid_key, pid, uid)
 
     user_key = 'user:{uid}'.format(uid=uid)
 
     pipe = conn.pipeline()
     pipe.hset(user_key, 'source', source)
-    pipe.hset(user_key, 'platform_uid', platform_uid)
+    pipe.hset(user_key, 'platform_uid', platform_user['id'])
     pipe.hset(user_key, 'access_token', access_token)
     pipe.hset(user_key, 'name', platform_user['screen_name'])
     pipe.execute()
@@ -63,6 +59,12 @@ def login():
     # Storing cookies.
     resp = make_response()
     resp.set_cookie('uid', uid, max_age=864000)
+    return resp
+
+@app.route('/logout')
+def logout():
+    resp = make_response(redirect('/'))
+    resp.set_cookie('uid', '', max_age=0)
     return resp
 
 @app.route('/wb_callback')
